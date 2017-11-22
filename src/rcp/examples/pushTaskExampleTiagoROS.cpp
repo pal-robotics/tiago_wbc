a08f6cc0e08e326892df155767bdddab97f764a1
Hilario Tome <hilario.tome@pal-robotics.com>
2017-11-22 11:07:20 +0100

    formating

--- src/rcp/examples/pushTaskExampleTiagoROS.cpp
+++ src/rcp/examples/pushTaskExampleTiagoROS.cpp
@@ -14,46 +14,54 @@
 #include <geometry_msgs/Pose.h>
 #include <pal_robot_tools/conversions.h>
 
-std::string generateTaskDescription(const property_bag::PropertyBag properties){
-
+std::string generateTaskDescription(const property_bag::PropertyBag properties)
+{
   std::stringstream ss;
   boost::archive::text_oarchive oa(ss);
   oa << properties;
   return ss.str();
 }
 
-int main(int argc, char** argv){
-
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "wbc_reemc_hardware_test");
   ros::NodeHandle nh("~");
 
-  ros::ServiceClient stackDescriptionServ = nh.serviceClient<pal_wbc_msgs::GetStackDescription>("/whole_body_kinematic_controller/get_stack_description");
-  ros::ServiceClient popTaskServ = nh.serviceClient<pal_wbc_msgs::PopTask>("/whole_body_kinematic_controller/pop_task");
-  ros::ServiceClient pushTaskServ = nh.serviceClient<pal_wbc_msgs::PushTask>("/whole_body_kinematic_controller/push_task");
-  ros::ServiceClient pushPopTaskServ = nh.serviceClient<pal_wbc_msgs::PushPopTask>("/whole_body_kinematic_controller/push_pop_task");
-  ros::ServiceClient getTaskErrorServ = nh.serviceClient<pal_wbc_msgs::GetTaskError>("/whole_body_kinematic_controller/get_task_error");
+  ros::ServiceClient stackDescriptionServ = nh.serviceClient<pal_wbc_msgs::GetStackDescription>(
+      "/whole_body_kinematic_controller/get_stack_description");
+  ros::ServiceClient popTaskServ = nh.serviceClient<pal_wbc_msgs::PopTask>(
+      "/whole_body_kinematic_controller/pop_task");
+  ros::ServiceClient pushTaskServ = nh.serviceClient<pal_wbc_msgs::PushTask>(
+      "/whole_body_kinematic_controller/push_task");
+  ros::ServiceClient pushPopTaskServ = nh.serviceClient<pal_wbc_msgs::PushPopTask>(
+      "/whole_body_kinematic_controller/push_pop_task");
+  ros::ServiceClient getTaskErrorServ = nh.serviceClient<pal_wbc_msgs::GetTaskError>(
+      "/whole_body_kinematic_controller/get_task_error");
 
-    std::string signal_type = "pointer_reflexx_typeII";
-//  std::string signal_type = "pointer";
+  std::string signal_type = "pointer_reflexx_typeII";
+  //  std::string signal_type = "pointer";
   bool blend = false;
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
   }
 
   {
-    //Push the goto position right arm task
+    // Push the goto position right arm task
     ROS_INFO_STREAM("Pushing new gotoPosition task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GoToPositionMetaTask"));
+    property_bag::PropertyBag taskProperties("taskType",
+                                             std::string("pal_wbc/GoToPositionMetaTask"));
     Eigen::Vector3d positionGoal(0.54637, -0.13707, 0.95628);
     geometry_msgs::PointStamped position_goal_msg;
     pal::convert(positionGoal, position_goal_msg.point);
@@ -70,28 +78,31 @@ int main(int argc, char** argv){
     srv.request.push_task_params.respect_task_id = "self_collision";
     srv.request.push_task_params.order.order = pal_wbc_msgs::Order::After;
     srv.request.push_task_params.blend = blend;
-    if (pushTaskServ.call(srv)){
+    if (pushTaskServ.call(srv))
+    {
       ROS_INFO_STREAM("Succesfully pushed task:");
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
 
@@ -100,42 +111,48 @@ int main(int argc, char** argv){
     pal_wbc_msgs::GetTaskError srv;
     srv.request.id = "go_to_position";
     getTaskErrorServ.call(srv);
-    ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    ROS_INFO_STREAM("Task: " << srv.request.id
+                             << " error norm is: " << srv.response.taskError.error_norm);
     int cont = 0;
-    while(srv.response.taskError.error_norm > 1e-2){
-      ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    while (srv.response.taskError.error_norm > 1e-2)
+    {
+      ROS_INFO_STREAM("Task: " << srv.request.id
+                               << " error norm is: " << srv.response.taskError.error_norm);
       ros::Duration(0.1).sleep();
       getTaskErrorServ.call(srv);
 
-      if(cont > 100){
+      if (cont > 100)
+      {
         ROS_ERROR_STREAM("Task did not converge");
         break;
       }
-      else{
+      else
+      {
         ++cont;
       }
     }
-
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
-    //Push the goto position right arm task
+    // Push the goto position right arm task
     ROS_INFO_STREAM("Pushing new gotoPosition task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GoToPositionMetaTask"));
+    property_bag::PropertyBag taskProperties("taskType",
+                                             std::string("pal_wbc/GoToPositionMetaTask"));
     Eigen::Vector3d positionGoal(0.74637, -0.13707, 0.95628);
 
     taskProperties.addProperty("task_id", std::string("go_to_position"));
@@ -151,28 +168,31 @@ int main(int argc, char** argv){
     push_params.order.order = pal_wbc_msgs::Order::Replace;
     push_params.blend = blend;
     srv.request.push_tasks.push_back(push_params);
-    if (pushPopTaskServ.call(srv)){
+    if (pushPopTaskServ.call(srv))
+    {
       ROS_INFO_STREAM("Succesfully pushed task:");
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
@@ -180,42 +200,48 @@ int main(int argc, char** argv){
     pal_wbc_msgs::GetTaskError srv;
     srv.request.id = "go_to_position";
     getTaskErrorServ.call(srv);
-    ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    ROS_INFO_STREAM("Task: " << srv.request.id
+                             << " error norm is: " << srv.response.taskError.error_norm);
     int cont = 0;
-    while(srv.response.taskError.error_norm > 1e-2){
-      ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    while (srv.response.taskError.error_norm > 1e-2)
+    {
+      ROS_INFO_STREAM("Task: " << srv.request.id
+                               << " error norm is: " << srv.response.taskError.error_norm);
       ros::Duration(0.1).sleep();
       getTaskErrorServ.call(srv);
 
-      if(cont > 100){
+      if (cont > 100)
+      {
         ROS_ERROR_STREAM("Task did not converge");
         break;
       }
-      else{
+      else
+      {
         ++cont;
       }
     }
-
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
-    //Push the goto position right arm task
+    // Push the goto position right arm task
     ROS_INFO_STREAM("Pushing new gotoPosition task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GoToPositionMetaTask"));
+    property_bag::PropertyBag taskProperties("taskType",
+                                             std::string("pal_wbc/GoToPositionMetaTask"));
     Eigen::Vector3d positionGoal(0.54637, -0.13707, 0.95628);
 
     taskProperties.addProperty("task_id", std::string("go_to_position"));
@@ -231,28 +257,31 @@ int main(int argc, char** argv){
     push_params.order.order = pal_wbc_msgs::Order::Replace;
     push_params.blend = blend;
     srv.request.push_tasks.push_back(push_params);
-    if (pushPopTaskServ.call(srv)){
+    if (pushPopTaskServ.call(srv))
+    {
       ROS_INFO_STREAM("Succesfully pushed task:");
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
@@ -260,47 +289,53 @@ int main(int argc, char** argv){
     pal_wbc_msgs::GetTaskError srv;
     srv.request.id = "go_to_position";
     getTaskErrorServ.call(srv);
-    ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    ROS_INFO_STREAM("Task: " << srv.request.id
+                             << " error norm is: " << srv.response.taskError.error_norm);
     int cont = 0;
-    while(srv.response.taskError.error_norm > 1e-2){
-      ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    while (srv.response.taskError.error_norm > 1e-2)
+    {
+      ROS_INFO_STREAM("Task: " << srv.request.id
+                               << " error norm is: " << srv.response.taskError.error_norm);
       ros::Duration(0.1).sleep();
       getTaskErrorServ.call(srv);
 
-      if(cont > 100){
+      if (cont > 100)
+      {
         ROS_ERROR_STREAM("Task did not converge");
         break;
       }
-      else{
+      else
+      {
         ++cont;
       }
     }
-
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
-    //Push the goto orientation task right arm task
+    // Push the goto orientation task right arm task
     ROS_INFO_STREAM("Pushing new GotoOrientation task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GoToOrientationMetaTask"));
+    property_bag::PropertyBag taskProperties(
+        "taskType", std::string("pal_wbc/GoToOrientationMetaTask"));
     Eigen::Quaterniond target_orientation;
 
     target_orientation.w() = 1.0;
     target_orientation.x() = 0.0;
-    target_orientation.y() =  0.0;
+    target_orientation.y() = 0.0;
     target_orientation.z() = 0.0;
 
     taskProperties.addProperty("task_id", std::string("go_to_orientation"));
@@ -310,34 +345,37 @@ int main(int argc, char** argv){
     taskProperties.addProperty("signal_reference", signal_type);
 
     pal_wbc_msgs::PushTask srv;
-    srv.request.push_task_params.params = generateTaskDescription(taskProperties);;
+    srv.request.push_task_params.params = generateTaskDescription(taskProperties);
+    ;
     srv.request.push_task_params.respect_task_id = "go_to_position";
     srv.request.push_task_params.order.order = pal_wbc_msgs::Order::After;
     srv.request.push_task_params.blend = blend;
 
-    if (pushTaskServ.call(srv)){
+    if (pushTaskServ.call(srv))
+    {
       ROS_INFO_STREAM("Succesfully pushed task:");
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
-    //Get current stack description
+    // Get current stack description
     pal_wbc_msgs::GetStackDescription statusSrv;
-    if (stackDescriptionServ.call(statusSrv)){
+    if (stackDescriptionServ.call(statusSrv))
+    {
       ROS_INFO_STREAM("Stack description:");
       ROS_INFO_STREAM(statusSrv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
@@ -345,28 +383,33 @@ int main(int argc, char** argv){
     pal_wbc_msgs::GetTaskError srv;
     srv.request.id = "go_to_orientation";
     getTaskErrorServ.call(srv);
-    ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    ROS_INFO_STREAM("Task: " << srv.request.id
+                             << " error norm is: " << srv.response.taskError.error_norm);
     int cont = 0;
-    while(srv.response.taskError.error_norm > 1e-2){
-      ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    while (srv.response.taskError.error_norm > 1e-2)
+    {
+      ROS_INFO_STREAM("Task: " << srv.request.id
+                               << " error norm is: " << srv.response.taskError.error_norm);
       ros::Duration(0.1).sleep();
       getTaskErrorServ.call(srv);
 
-      if(cont > 100){
+      if (cont > 100)
+      {
         ROS_ERROR_STREAM("Task did not converge");
         break;
       }
-      else{
+      else
+      {
         ++cont;
       }
-
     }
   }
 
   {
-    //Push gaze
+    // Push gaze
     ROS_INFO_STREAM("Pushing new gaze task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GazePointKinematicMetaTask"));
+    property_bag::PropertyBag taskProperties(
+        "taskType", std::string("pal_wbc/GazePointKinematicMetaTask"));
     Eigen::Vector3d positionGoal(2, 0, 1.0);
 
     taskProperties.addProperty("task_id", std::string("gaze"));
@@ -381,15 +424,16 @@ int main(int argc, char** argv){
     srv.request.push_task_params.order.order = pal_wbc_msgs::Order::After;
     srv.request.push_task_params.blend = blend;
 
-    if (pushTaskServ.call(srv)){
+    if (pushTaskServ.call(srv))
+    {
       ROS_INFO_STREAM("Succesfully pushed task:");
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
-
   }
 
   {
@@ -397,34 +441,40 @@ int main(int argc, char** argv){
     pal_wbc_msgs::GetTaskError srv;
     srv.request.id = "gaze";
     getTaskErrorServ.call(srv);
-    ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    ROS_INFO_STREAM("Task: " << srv.request.id
+                             << " error norm is: " << srv.response.taskError.error_norm);
     int cont = 0;
-    while(srv.response.taskError.error_norm > 1e-2){
-      ROS_INFO_STREAM("Task: "<<srv.request.id<<" error norm is: "<<srv.response.taskError.error_norm);
+    while (srv.response.taskError.error_norm > 1e-2)
+    {
+      ROS_INFO_STREAM("Task: " << srv.request.id
+                               << " error norm is: " << srv.response.taskError.error_norm);
       ros::Duration(0.1).sleep();
       getTaskErrorServ.call(srv);
 
-      if(cont > 100){
+      if (cont > 100)
+      {
         ROS_ERROR_STREAM("Task did not converge");
         break;
       }
-      else{
+      else
+      {
         ++cont;
       }
-
     }
   }
 
-  //Pop the gaze task right arm
+  // Pop the gaze task right arm
   {
     ROS_INFO_STREAM("Poping orientation Task");
     pal_wbc_msgs::PopTask srv;
     srv.request.name = "gaze";
     srv.request.blend = blend;
-    if (popTaskServ.call(srv)){
+    if (popTaskServ.call(srv))
+    {
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
@@ -433,17 +483,19 @@ int main(int argc, char** argv){
   /// @bug
   ros::Duration(5.).sleep();
 
-  //Pop the goto orientation task right arm
+  // Pop the goto orientation task right arm
   {
     ROS_INFO_STREAM("Poping orientation Task");
     pal_wbc_msgs::PopTask srv;
     srv.request.name = "go_to_orientation";
     srv.request.blend = blend;
 
-    if (popTaskServ.call(srv)){
+    if (popTaskServ.call(srv))
+    {
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
@@ -452,17 +504,19 @@ int main(int argc, char** argv){
   /// @bug
   ros::Duration(5.0).sleep();
 
-  //Pop the go to position task right arm
+  // Pop the go to position task right arm
   {
     ROS_INFO_STREAM("Poping position Task");
     pal_wbc_msgs::PopTask srv;
     srv.request.name = "go_to_position";
     srv.request.blend = blend;
 
-    if (popTaskServ.call(srv)){
+    if (popTaskServ.call(srv))
+    {
       ROS_INFO_STREAM(srv.response);
     }
-    else{
+    else
+    {
       ROS_ERROR("Failed to call service ");
       return 1;
     }
@@ -474,7 +528,8 @@ int main(int argc, char** argv){
   {
     //Push the goto position right arm task
     ROS_INFO_STREAM("Pushing new gotoPosition task");
-    property_bag::PropertyBag taskProperties("taskType", std::string("pal_wbc/GoToPositionMetaTask"));
+    property_bag::PropertyBag taskProperties("taskType",
+  std::string("pal_wbc/GoToPositionMetaTask"));
 
     ///////////////
     taskProperties.addProperty("task_id", std::string("go_to_position"));
@@ -499,5 +554,4 @@ int main(int argc, char** argv){
   */
 
   return 0;
-
 }
--- src/tiago_rcp_stack.cpp
+++ src/tiago_rcp_stack.cpp
@@ -2,7 +2,7 @@
 #include <rbdl/addons/urdfreader/urdfreader.h>
 
 #include <pal_wbc_controller/task_abstract.h>
-#include <pal_wbc_controller/StackOfTasksKinematic.h>
+#include <pal_wbc_controller/stack_of_tasks_kinematic.h>
 
 #include <wbc_tasks/joint_pos_limit_kinematic_task.h>
 #include <wbc_tasks/constraint_kinematic_task.h>
@@ -18,12 +18,13 @@
 
 using namespace pal_wbc;
 
-class tiago_rcp_stack: public StackConfigurationKinematic{
-  bool setupStack(StackOfTasksKinematicPtr stack, ros::NodeHandle &nh){
-
+class tiago_rcp_stack : public StackConfigurationKinematic
+{
+  bool setupStack(StackOfTasksKinematicPtr stack, ros::NodeHandle &nh)
+  {
     ROS_DEBUG_STREAM("Loading tiago rpc stack");
 
-    std::vector< double > joint_pos_min_override = stack->getJointPositionLimitMin();
+    std::vector<double> joint_pos_min_override = stack->getJointPositionLimitMin();
     joint_pos_min_override[stack->getJointIndex("arm_4_joint")] = 0.2;
 
     std::vector<std::string> default_reference_joints;
@@ -40,21 +41,27 @@ class tiago_rcp_stack: public StackConfigurationKinematic{
 
     Eigen::VectorXd default_reference_posture(default_reference_joints.size());
     default_reference_posture.setZero();
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_1_joint"))) = 0.11;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_2_joint"))) = -0.51;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_3_joint"))) = -3.22;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_4_joint"))) = 2.0;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_5_joint"))) = 1.91;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_6_joint"))) = 0.36;
-    default_reference_posture(indexVectorThrow(default_reference_joints, std::string("arm_7_joint"))) = 0.3;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_1_joint"))) = 0.11;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_2_joint"))) = -0.51;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_3_joint"))) = -3.22;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_4_joint"))) = 2.0;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_5_joint"))) = 1.91;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_6_joint"))) = 0.36;
+    default_reference_posture(
+        indexVectorThrow(default_reference_joints, std::string("arm_7_joint"))) = 0.3;
 
     // 1. Joint and velocity limits
     JointPositionLimitKinematicAllJointsMetaTaskPtr joint_position_limit_task(
-          new JointPositionLimitKinematicAllJointsMetaTask(*stack.get(),
-                                                           joint_pos_min_override, stack->getJointPositionLimitMax(),
-                                                           stack->getJointVelocityLimitMin(), stack->getJointVelocityLimitMax(),
-                                                           stack->getJointNames(),
-                                                           1.0, false, nh));
+        new JointPositionLimitKinematicAllJointsMetaTask(
+            *stack.get(), joint_pos_min_override, stack->getJointPositionLimitMax(),
+            stack->getJointVelocityLimitMin(), stack->getJointVelocityLimitMax(),
+            stack->getJointNames(), 1.0, false, nh));
 
     stack->pushTask("joint_limits", joint_position_limit_task);
 
@@ -65,17 +72,15 @@ class tiago_rcp_stack: public StackConfigurationKinematic{
     sc_params.epsison = 0.02;
     sc_params.safety_distance = 0;
     sc_params.number_collisions = 10;
-    SelfCollisionSafetyKinematicTaskPtr self_collision(new SelfCollisionSafetyKinematicTask() );
+    SelfCollisionSafetyKinematicTaskPtr self_collision(new SelfCollisionSafetyKinematicTask());
     self_collision->setUpTask(sc_params, *stack.get(), nh);
     self_collision->setDamping(0.1);
 
     stack->pushTask("self_collision", self_collision);
 
     ReferenceKinematicTaskAllJointsMetaTaskPtr default_reference(
-          new ReferenceKinematicTaskAllJointsMetaTask(*stack.get(),
-                                                      default_reference_joints,
-                                                      default_reference_posture,
-                                                      nh, 0.5));
+        new ReferenceKinematicTaskAllJointsMetaTask(*stack.get(), default_reference_joints,
+                                                    default_reference_posture, nh, 0.5));
     stack->pushTask("rest_joint_configuration", default_reference);
 
 
